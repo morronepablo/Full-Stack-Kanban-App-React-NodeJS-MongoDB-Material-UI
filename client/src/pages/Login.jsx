da15ab2c7a0364fbf646da1fd125e340c1b01314
morronepablo <morronepablo@gmail.com>
2024-01-17 19:47:43 -0300

    Maquetacion y ajustes en login y signup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
+  return (
+    <>
+      <Box component={"form"} sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          disabled={loading}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          label="Password"
+          name="password"
+          type="password"
+          disabled={loading}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          variant="outlined"
+          fullWidth
+          color="success"
+          type="submit"
+          loading={loading}
+        >
+          Login
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/signup" sx={{ textTransform: "none" }}>
+        Don't have an account? Signup
+      </Button>
+    </>
+  );
 };
 
 export default Login;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
+  return (
+    <>
+      <Box component={"form"} sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          disabled={loading}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          label="Password"
+          name="password"
+          type="password"
+          disabled={loading}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="confirmPassword"
+          label="Confirm Password"
+          name="confirmPassword"
+          type="password"
+          disabled={loading}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          variant="outlined"
+          fullWidth
+          color="success"
+          type="submit"
+          loading={loading}
+        >
+          Sign Up
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/login" sx={{ textTransform: "none" }}>
+        Already have an account? Login
+      </Button>
+    </>
+  );
 };
 
 export default Signup;
