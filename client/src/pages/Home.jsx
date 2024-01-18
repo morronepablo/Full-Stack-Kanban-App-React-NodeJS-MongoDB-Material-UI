d3deaafbd61f58d930fb9d0945a4143cd9fda60b
morronepablo <morronepablo@gmail.com>
2024-01-18 03:07:12 -0300

    Finality Functions Login and Signup and create sidebar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { Box } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
 
 const Home = () => {
-  return <div>Home</div>;
+  const createBoard = () => {};
+
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <LoadingButton variant="outlined" color="success" onClick={createBoard}>
+        Click here to create your first board
+      </LoadingButton>
+    </Box>
+  );
 };
 
 export default Home;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,16 +2,79 @@ import { Box, Button, TextField } from "@mui/material";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import LoadingButton from "@mui/lab/LoadingButton";
+import authApi from "../api/authApi";
 
 const Signup = () => {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => {};
+  const [loading, setLoading] = useState(false);
+  const [usernameErrText, setUsernameErrText] = useState("");
+  const [passwordErrText, setPasswordErrText] = useState("");
+  const [confirmPasswordErrText, setConfirmPasswordErrText] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUsernameErrText("");
+    setPasswordErrText("");
+    setConfirmPasswordErrText("");
+
+    const data = new FormData(e.target);
+    const username = data.get("username").trim();
+    const password = data.get("password").trim();
+    const confirmPassword = data.get("confirmPassword").trim();
+
+    let err = false;
+
+    if (username === "") {
+      err = true;
+      setUsernameErrText("Please fill this field");
+    }
+    if (password === "") {
+      err = true;
+      setPasswordErrText("Please fill this field");
+    }
+    if (confirmPassword === "") {
+      err = true;
+      setConfirmPasswordErrText("Please fill this field");
+    }
+    if (password !== confirmPassword) {
+      err = true;
+      setConfirmPasswordErrText("Confirm password not match");
+    }
+
+    if (err) return;
+
+    setLoading(true);
+
+    try {
+      const res = await authApi.signup({
+        username,
+        password,
+        confirmPassword,
+      });
+      setLoading(false);
+      localStorage.setItem("token", res.token);
+      navigate("/");
+    } catch (err) {
+      const errors = err.data.errors;
+      errors.forEach((e) => {
+        if (e.param === "username") {
+          setUsernameErrText(e.msg);
+        }
+        if (e.param === "password") {
+          setPasswordErrText(e.msg);
+        }
+        if (e.param === "confirmPassword") {
+          setConfirmPasswordErrText(e.msg);
+        }
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <>
-      <Box component={"form"} sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
+      <Box component="form" sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
         <TextField
           margin="normal"
           required
@@ -20,6 +83,8 @@ const Signup = () => {
           label="Username"
           name="username"
           disabled={loading}
+          error={usernameErrText !== ""}
+          helperText={usernameErrText}
         />
         <TextField
           margin="normal"
@@ -30,6 +95,8 @@ const Signup = () => {
           name="password"
           type="password"
           disabled={loading}
+          error={passwordErrText !== ""}
+          helperText={passwordErrText}
         />
         <TextField
           margin="normal"
@@ -40,6 +107,8 @@ const Signup = () => {
           name="confirmPassword"
           type="password"
           disabled={loading}
+          error={confirmPasswordErrText !== ""}
+          helperText={confirmPasswordErrText}
         />
         <LoadingButton
           sx={{ mt: 3, mb: 2 }}
@@ -49,7 +118,7 @@ const Signup = () => {
           type="submit"
           loading={loading}
         >
-          Sign Up
+          Signup
         </LoadingButton>
       </Box>
       <Button component={Link} to="/login" sx={{ textTransform: "none" }}>
