a0be103b137da1fa1a9c78381434124312292e0e
morronepablo <morronepablo@gmail.com>
2024-01-18 21:36:56 -0300

    Proyecto Finalizado

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -5,22 +5,79 @@ import {
   IconButton,
   List,
   ListItem,
+  ListItemButton,
   Typography,
 } from "@mui/material";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import assets from "../../assets/index";
+import { useEffect, useState } from "react";
+import boardApi from "../../api/boardApi";
+import { setBoards } from "../../redux/features/boardSlice";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import FavouriteList from "./FavouriteList";
 
 const Sidebar = () => {
   const user = useSelector((state) => state.user.value);
+  const boards = useSelector((state) => state.board.value);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { boardId } = useParams();
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const sidebarWidth = 250;
+
+  useEffect(() => {
+    const getBoards = async () => {
+      try {
+        const res = await boardApi.getAll();
+        dispatch(setBoards(res));
+      } catch (err) {
+        alert(err);
+      }
+    };
+    getBoards();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const activeItem = boards.findIndex((e) => e.id === boardId);
+    if (boards.length > 0 && boardId === undefined) {
+      navigate(`/boards/${boards[0].id}`);
+    }
+    setActiveIndex(activeItem);
+  }, [boards, boardId, navigate]);
 
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/login");
+  };
+
+  const onDragEnd = async ({ source, destination }) => {
+    const newList = [...boards];
+    const [removed] = newList.splice(source.index, 1);
+    newList.splice(destination.index, 0, removed);
+
+    const activeItem = newList.findIndex((e) => e.id === boardId);
+    setActiveIndex(activeItem);
+    dispatch(setBoards(newList));
+
+    try {
+      await boardApi.updatePositoin({ boards: newList });
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const addBoard = async () => {
+    try {
+      const res = await boardApi.create();
+      const newList = [res, ...boards];
+      dispatch(setBoards(newList));
+      navigate(`/boards/${res.id}`);
+    } catch (err) {
+      alert(err);
+    }
   };
 
   return (
@@ -51,7 +108,7 @@ const Sidebar = () => {
               justifyContent: "space-between",
             }}
           >
-            <Typography variant="body2" fontWeight={"700"}>
+            <Typography variant="body2" fontWeight="700">
               {user.username}
             </Typography>
             <IconButton onClick={logout}>
@@ -60,20 +117,7 @@ const Sidebar = () => {
           </Box>
         </ListItem>
         <Box sx={{ paddingTop: "10px" }} />
-        <ListItem>
-          <Box
-            sx={{
-              width: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Typography variant="body2" fontWeight={"700"}>
-              Favorites
-            </Typography>
-          </Box>
-        </ListItem>
+        <FavouriteList />
         <Box sx={{ paddingTop: "10px" }} />
         <ListItem>
           <Box
@@ -84,14 +128,58 @@ const Sidebar = () => {
               justifyContent: "space-between",
             }}
           >
-            <Typography variant="body2" fontWeight={"700"}>
+            <Typography variant="body2" fontWeight="700">
               Private
             </Typography>
-            <IconButton>
+            <IconButton onClick={addBoard}>
               <AddBoxOutlinedIcon fontSize="small" />
             </IconButton>
           </Box>
         </ListItem>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable
+            key={"list-board-droppable-key"}
+            droppableId={"list-board-droppable"}
+          >
+            {(provided) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {boards.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <ListItemButton
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        selected={index === activeIndex}
+                        component={Link}
+                        to={`/boards/${item.id}`}
+                        sx={{
+                          pl: "20px",
+                          cursor: snapshot.isDragging
+                            ? "grab"
+                            : "pointer!important",
+                        }}
+                      >
+                        <Typography
+                          variant="body2"
+                          fontWeight="700"
+                          sx={{
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                          }}
+                        >
+                          {item.icon} {item.title}
+                        </Typography>
+                      </ListItemButton>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </List>
     </Drawer>
   );
